02962059c3022ffc501bd0b04c4ddf149ac9fd7d
Niharika Sharma <niharika_sharma>
2010-07-23 04:55:19 +0000

    The login processor.
    SVN-Revision: 7

---
+++ src/edu/wustl/processor/LoginProcessor.java
@@ -0,0 +1,108 @@
+package edu.wustl.processor;
+
+import java.util.List;
+
+import edu.wustl.authmanager.CSMAuthManager;
+import edu.wustl.authmanager.IDPAuthManager;
+import edu.wustl.authmanager.LDAPAuthManager;
+import edu.wustl.authmanager.LoginResult;
+import edu.wustl.authmanager.MigrationState;
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.domain.UserDetails;
+import edu.wustl.migrator.util.Utility;
+
+public class LoginProcessor
+{
+    public static LoginResult processUserLogin(final String loginName, final String password)
+    {
+        final LoginResult loginResult;
+        final UserDetails userDetails = getUserDetails(loginName);
+        if (userDetails == null)
+        {
+            loginResult = processUserLoginWithoutMigrationInfo(loginName, password);
+        }
+        else
+        {
+            loginResult = processUserLoginWithMigrationInfo(loginName, password, userDetails);
+        }
+        return loginResult;
+    }
+
+    private static LoginResult processUserLoginWithoutMigrationInfo(final String loginName, final String password)
+    {
+        final LoginResult loginResult = new LoginResult();
+        try
+        {
+            final String queryStr = "SELECT * FROM CSM_USER WHERE LOGIN_NAME = '" + loginName + "'";
+            final List<List<String>> resultList = Utility.executeQueryUsingDataSource(queryStr, false, "WUSTLKey");
+            if (resultList == null)
+            {
+                // New user
+                loginResult.setMigrationState(MigrationState.NEW_WUSTL_USER);
+                loginResult.setAuthenticationSuccess(true);
+
+            }
+            else
+            {
+                final IDPAuthManager authManager = new CSMAuthManager();
+                loginResult.setAuthenticationSuccess(authManager.authenticate(loginName, password));
+                loginResult.setMigrationState(MigrationState.TO_BE_MIGRATED);
+            }
+        }
+        catch (final ApplicationException e)
+        {
+            //
+        }
+        loginResult.setLoginId(loginName);
+        return loginResult;
+    }
+
+    private static LoginResult processUserLoginWithMigrationInfo(final String loginName, final String password,
+            final UserDetails userDetails)
+    {
+        final LoginResult loginResult = new LoginResult();
+        final IDPAuthManager authManager;
+        if (MigrationState.MIGRATED.equals(userDetails.getMigrationState()))
+        {
+            loginResult.setMigrationState(MigrationState.MIGRATED);
+            loginResult.setLoginId(userDetails.getMigratedLoginName());
+            authManager = new LDAPAuthManager();
+        }
+        else
+        {
+            loginResult.setLoginId(userDetails.getLoginName());
+            loginResult.setMigrationState(MigrationState.DO_NOT_MIGRATE);
+            authManager = new CSMAuthManager();
+        }
+        loginResult.setAuthenticationSuccess(authManager.authenticate(loginName, password));
+        return loginResult;
+    }
+
+    private static UserDetails getUserDetails(final String loginName)
+    {
+        UserDetails userDetails = null;
+        try
+        {
+            final String queryStr = "SELECT LOGIN_NAME, TARGET_IDP_NAME, MIGRATED_LOGIN_NAME,MIGRATION_STATUS FROM CSM_MIGRATE_USER WHERE MIGRATED_LOGIN_NAME = '"
+                    + loginName + "' or LOGIN_NAME='" + loginName + "'";
+
+            final List<List<String>> resultList = Utility.executeQueryUsingDataSource(queryStr, false, "WUSTLKey");
+
+            if (resultList != null)
+            {
+                userDetails = new UserDetails();
+                final List<String> userDetailsRecord = resultList.get(0);
+                userDetails.setLoginName(userDetailsRecord.get(0).toString());
+                userDetails.setTargetIDP(userDetailsRecord.get(1).toString());
+                userDetails.setMigratedLoginName(userDetailsRecord.get(2).toString());
+                userDetails.setMigrationState(MigrationState.get(userDetailsRecord.get(3).toString()));
+            }
+        }
+        catch (final ApplicationException appException)
+        {
+            // throw new MigratorException(appException);
+        }
+        return userDetails;
+    }
+
+}
