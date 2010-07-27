c218d760839fe0fae4ac3bd02d9dec20fd491ed0
Niharika Sharma <niharika_sharma>
2010-07-27 10:55:33 +0000

    Utility class  containing utility method to perform database queries.
    SVN-Revision: 26

--- src/edu/wustl/migrator/util/Utility.java
+++ src/edu/wustl/migrator/util/Utility.java
@@ -7,8 +7,30 @@ import edu.wustl.dao.JDBCDAO;
 import edu.wustl.dao.daofactory.DAOConfigFactory;
 import edu.wustl.dao.exception.DAOException;
 
+/**
+ * This class contains the common methods required by the Migrator classes.
+ *
+ * @author niharika_sharma
+ *
+ */
 public class Utility
 {
+
+    /**
+     * Execute query using data source.
+     *
+     * @param queryString
+     *            the query string
+     * @param isDML
+     *            the is dml
+     * @param applicationName
+     *            the application name
+     *
+     * @return the list
+     *
+     * @throws ApplicationException
+     *             the application exception
+     */
     public static List executeQueryUsingDataSource(final String queryString, final Boolean isDML,
             final String applicationName) throws ApplicationException
     {
@@ -33,7 +55,7 @@ public class Utility
         }
         catch (final DAOException ex)
         {
-            //logger.debug(ex.getMessage(), ex);
+            // logger.debug(ex.getMessage(), ex);
             jdbcDAO.rollback();
             throw new ApplicationException(ex.getErrorKey(), ex, ex.getMessage());
         }
