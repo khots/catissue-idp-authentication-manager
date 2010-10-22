893a2cb67b0b52f086c2dfe34a50efe547b98a6e
Chitra Garg <gargc>
2010-10-22 12:53:19 +0000

    restructured the code as per BDA
    SVN-Revision: 65

---
+++ trunk/software/IDPAuthenticaionManager/src/java/edu/wustl/domain/LoginCredentials.java
@@ -0,0 +1,28 @@
+package edu.wustl.domain;
+
+public class LoginCredentials
+{
+    private String loginName;
+
+    private String password;
+
+    public String getLoginName()
+    {
+        return loginName;
+    }
+
+    public void setLoginName(final String loginName)
+    {
+        this.loginName = loginName;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(final String password)
+    {
+        this.password = password;
+    }
+}
