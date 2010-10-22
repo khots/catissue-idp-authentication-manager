ec199161dc70ba71f8fd791063f934a4d7af4c04
Chitra Garg <gargc>
2010-10-22 12:53:19 +0000

    restructured the code as per BDA
    SVN-Revision: 65

---
+++ software/IDPAuthenticaionManager/src/java/edu/wustl/authmanager/IDPAuthManager.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package edu.wustl.authmanager;
+
+import edu.wustl.auth.exception.AuthenticationException;
+import edu.wustl.domain.LoginCredentials;
+import edu.wustl.idp.IDPInterface;
+
+/**
+ * @author supriya_dankh
+ *
+ */
+public interface IDPAuthManager {
+
+	public boolean authenticate(LoginCredentials credentials)throws AuthenticationException;
+
+	IDPInterface getIDP();
+
+
+}
