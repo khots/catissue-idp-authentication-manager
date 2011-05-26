8b7c26bc5deb8aa6209ee3e7f8ff4b1a10c7456d
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-26 14:06:48 +0000

    branch for catissue 1.2.1.wu release
    SVN-Revision: 84

---
+++ branches/IDP_1.2.1.wu/IDP_v10_RC2_15OCT10_TAG/src/edu/wustl/authmanager/CSMAuthManager.java
@@ -0,0 +1,53 @@
+package edu.wustl.authmanager;
+
+import edu.wustl.auth.exception.AuthenticationException;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.domain.LoginCredentials;
+import edu.wustl.idp.IDPInterface;
+import edu.wustl.security.exception.SMException;
+import edu.wustl.security.manager.SecurityManagerFactory;
+
+/**
+ * @author supriya_dankh
+ *
+ */
+public class CSMAuthManager extends IDPAuthManagerImpl
+{
+
+    private static final Logger LOGGER = Logger.getCommonLogger(CSMAuthManager.class);
+
+    public CSMAuthManager(final IDPInterface abstratIdp)
+    {
+        super(abstratIdp);
+        // TODO Auto-generated constructor stub
+    }
+
+    public CSMAuthManager()
+    {
+
+    }
+
+    protected void initialize()
+    {
+
+    }
+
+    public boolean authenticate(final LoginCredentials loginCredentials) throws AuthenticationException
+    {
+        boolean loginSuccess = false;
+
+        try
+        {
+            loginSuccess = SecurityManagerFactory.getSecurityManager().login(loginCredentials.getLoginName(),
+                    loginCredentials.getPassword());
+        }
+        catch (final SMException e)
+        {
+            LOGGER.debug(e);
+            throw new AuthenticationException(e);
+        }
+
+        return loginSuccess;
+    }
+
+}
