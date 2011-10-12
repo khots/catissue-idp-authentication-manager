15c96666923c2c11757261b48799149174499a57
asrinivasa <asrinivasa>
2011-10-12 14:30:11 +0000

    using dorian url from published properties file
    SVN-Revision: 106

--- software/IDPAuthenticaionManager/src/main/java/edu/wustl/authmanager/CAGridAuthManager.java
+++ software/IDPAuthenticaionManager/src/main/java/edu/wustl/authmanager/CAGridAuthManager.java
@@ -1,5 +1,10 @@
 package edu.wustl.authmanager;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.naming.NamingException;
 
 import org.globus.gsi.GlobusCredential;
@@ -7,7 +12,6 @@ import org.globus.gsi.GlobusCredential;
 import edu.wustl.auth.exception.AuthenticationException;
 import edu.wustl.domain.LoginCredentials;
 import edu.wustl.idp.IDPInterface;
-import edu.wustl.migrator.util.Constants;
 import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
 
 public class CAGridAuthManager extends IDPAuthManagerImpl
@@ -20,37 +24,26 @@ public class CAGridAuthManager extends IDPAuthManagerImpl
 	 public CAGridAuthManager()
 	    {
 	    }
-	 private String authenticationURL = "";
 	 private String dorianUrl = "";
-	 private String syncDescFile="";
 
-	  /**
-	     * This method initializes LDAP context.
-	     *
-	     * @throws NamingException
-	     *             Throws naming exception in case of any error while
-	     *             initializing.
-	     */
-//	 public static void main(String[] args)
-//	 {
-//		 LoginCredentials credentials = new LoginCredentials();
-//		 credentials.setLoginName("nitesh");
-//		 credentials.setPassword("Haanji^$!6");
-//		 try {
-//			authenticate(credentials);
-//		} catch (AuthenticationException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	 }
-	    protected void initialize() throws NamingException
+	 public static Properties serviceUrls() {			
+			String serviceUrlsFile = System.getProperty("grid.service.urlsFilePath");
+			
+			InputStream in = null;
+			Properties serviceUrls = new Properties();
+			try {
+				in = new FileInputStream(serviceUrlsFile);
+				serviceUrls.load(in);
+			} catch (IOException e) {
+				e.printStackTrace();
+				serviceUrls = null;		
+			}
+			return serviceUrls;
+		}
+		
+	 	protected void initialize(final LoginCredentials loginCredentials) throws NamingException
 	    {
-	    	 authenticationURL =idp.getIDPProperties().get(Constants.AUTH_SERVICE_URL).toString();
-	    	 //"https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian";
-//	    	 dorianUrl = "https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian";
-	    	 dorianUrl = idp.getIDPProperties().get(Constants.DORIAN_SERVICE_URL).toString();
-	    	 syncDescFile = idp.getIDPProperties().getProperty(Constants.SYNC_DESC_FILE);
-
+	    	 dorianUrl = serviceUrls().getProperty("cagrid.master.dorian.service.url");
 	    }
 
 	    /**
@@ -61,12 +54,8 @@ public class CAGridAuthManager extends IDPAuthManagerImpl
 
 	        try
 	        {
-	            initialize();
-//	            synchronizePeriodic("");
-//	            synchronizeOnce("");
-	            GridAuthenticationClient.synchronizeOnce(syncDescFile);
-	            GridAuthenticationClient.authenticate(loginCredentials, dorianUrl, authenticationURL);
-//	            validateUser(loginCredentials, dorianUrl, authenticationURL);
+	            initialize(loginCredentials);
+	            GridAuthenticationClient.authenticate(loginCredentials, dorianUrl, dorianUrl);
 	        }
 	        catch (final Exception e)
 	        {
@@ -80,15 +69,11 @@ public class CAGridAuthManager extends IDPAuthManagerImpl
 			String identity = null;
 			try
 	        {
-	            initialize();
-//	            synchronizePeriodic("");
-//	            synchronizeOnce("");
-	            GridAuthenticationClient.synchronizeOnce(syncDescFile);
-	            GlobusCredential credential = GridAuthenticationClient.authenticate(loginCredentials, dorianUrl, authenticationURL);
+	            initialize(loginCredentials);
+	            GlobusCredential credential = GridAuthenticationClient.authenticate(loginCredentials, dorianUrl, dorianUrl);
 	            if (credential != null) {
 	            	identity = credential.getIdentity();
 	            }
-//	            validateUser(loginCredentials, dorianUrl, authenticationURL);
 	        }
 	        catch (final Exception e)
 	        {
@@ -103,13 +88,8 @@ public class CAGridAuthManager extends IDPAuthManagerImpl
 			SAMLAssertion saml  = null;
 			try
 	        {
-	            initialize();
-//	            synchronizePeriodic("");
-//	            synchronizeOnce("");
-	            GridAuthenticationClient.synchronizeOnce(syncDescFile);
-	             saml = GridAuthenticationClient.getSAMLAssertion(loginCredentials, dorianUrl, authenticationURL);
-
-//	            validateUser(loginCredentials, dorianUrl, authenticationURL);
+	            initialize(loginCredentials);
+	             saml = GridAuthenticationClient.getSAMLAssertion(loginCredentials, dorianUrl, dorianUrl);
 	        }
 	        catch (final Exception e)
 	        {
@@ -117,117 +97,5 @@ public class CAGridAuthManager extends IDPAuthManagerImpl
 	        }
 	        return saml;
 		}
-
-//	    public static boolean synchronizePeriodic(String syncDescriptionFile)
-//		{
-//			boolean success = false;
-//			try
-//			{
-//				// Load Sync Description
-//				SyncDescription description = (SyncDescription) Utils
-//						.deserializeDocument(syncDescriptionFile,
-//								SyncDescription.class);
-//
-//				// Sync with the Trust Fabric Once
-//				SyncGTS.getInstance().syncAndResyncInBackground(description, success);//syncAndResync(description, success);
-//				success = true;
-//			}
-//			catch (Exception e)
-//			{
-//				e.printStackTrace();
-//			}
-//
-//			return success;
-//		}
-//		private GlobusCredential validateUser(LoginCredentials loginCredentials,String dorianURL, String authenticationURL) throws Exception
-//		{
-//			BasicAuthentication auth = new BasicAuthentication();
-//	        auth.setUserId(loginCredentials.getLoginName());
-//	        auth.setPassword(loginCredentials.getPassword());
-//
-//	        // Authenticate to the IdP (DorianIdP) using credential
-//
-//	        AuthenticationClient authClient = new AuthenticationClient(authenticationURL);
-//	        SAMLAssertion saml = authClient.authenticate(auth);
-//
-//	        // Requested Grid Credential lifetime (12 hours)
-//
-//	        CertificateLifetime lifetime = new CertificateLifetime();
-//	        lifetime.setHours(12);
-//
-//	        // Request PKI/Grid Credential
-//	        GridUserClient dorian = new GridUserClient(dorianURL);
-//	        GlobusCredential credential = dorian.requestUserCertificate(saml, lifetime);
-//	        return credential;
-//
-//		}
-////
-////		// From: http://cagrid.org/display/gts12/Programmatic+Approach
-////		// Purpose: obtain trusted CA certificates from Slave GTS service
-//		public static boolean synchronizeOnce(String syncDescriptionFile)
-//		{
-//
-//			boolean success = false;
-//			try
-//			{
-//				// Load Sync Description
-////				String pathToSyncDescription = "C:/sync-description.xml";
-//				SyncDescription description = (SyncDescription) Utils
-//						.deserializeDocument(syncDescriptionFile,
-//								SyncDescription.class);
-//
-//				// Sync with the Trust Fabric Once
-//				SyncGTS.getInstance().syncOnce(description);
-//				success = true;
-//			}
-//			catch (Exception e)
-//			{
-//				e.printStackTrace();
-//			}
-//
-//			return success;
-//		}
-//
-//
-//		private static boolean synchronizePeriodic(String syncDescriptionFile)
-//		{
-//			boolean success = false;
-//			try
-//			{
-//				// Load Sync Description
-//				String pathToSyncDescription = "C:/sync-description.xml"; // "conf/training/sync-description.xml";
-//				SyncDescription description = (SyncDescription) Utils
-//						.deserializeDocument(pathToSyncDescription,
-//								SyncDescription.class);
-//
-//				// Sync with the Trust Fabric Once
-//				SyncGTS.getInstance().syncAndResyncInBackground(description, success);//syncAndResync(description, success);
-//				success = true;
-//			}
-//			catch (Exception e)
-//			{
-//				e.printStackTrace();
-//			}
-//
-//			return success;
-//		}
-
-//		public static void main(String[] args)
-//		 {
-//			 LoginCredentials credentials = new LoginCredentials();
-//			 credentials.setLoginName("nitesh");
-//			 credentials.setPassword("Haanji^$!6");
-//			 try {
-//				authenticate(credentials,"https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian","https://dorian.training.cagrid.org:8443/wsrf/services/cagrid/Dorian");
-//			} catch (Exception e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//		 }
-
-
-
-
-
 
 }
